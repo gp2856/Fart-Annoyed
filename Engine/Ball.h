1fb45abcafab51189fc9ee9d1ccf23b49c6ac352
GP <gp2856@users.no-reply.github.com>
2017-12-12 02:56:23 -0600

    implemented ball class

---
+++ Engine/Ball.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "RectF.h"
+#include "Graphics.h"
+#include "Vec2.h"
+
+
+class Ball
+{
+public:
+	Ball(const Vec2& pos_in, const Vec2& vel_in);
+	void Draw(Graphics& gfx) const;
+	void Update();
+private:
+	static constexpr float radius = 7.0f;
+	Vec2 pos;
+	Vec2 vel;
+
+};
