564e5f32cfa79ae76215eaf1e1113b59c9ad1c09
GP <gp2856@users.no-reply.github.com>
2017-12-13 13:21:44 -0600

    bouncing balls

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -10,14 +10,14 @@ RectF::RectF(float left_in, float right_in, float top_in, float bottom_in)
 }
 
 RectF::RectF(const Vec2 & top_left, const Vec2 & bottom_right)
-{
-	RectF(top_left.x, bottom_right.x, top_left.y, bottom_right.y);
-}
+	:
+	RectF(top_left.x, bottom_right.x, top_left.y, bottom_right.y)
+{}
 
 RectF::RectF(const Vec2 & top_left, float width, float height)
-{
-	RectF(top_left, top_left + Vec2(width, height));
-}
+	:
+	RectF(top_left, top_left + Vec2(width, height))
+{}
 
 RectF RectF::FromCenter(const Vec2 & center, float halfWidth, float halfHeight)
 {
