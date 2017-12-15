570f0ac20bd5b079ce0e0c308e62d0b2f108a9d4
GP <gp2856@users.no-reply.github.com>
2017-12-15 10:26:12 -0600

    paddlin my balls

---
+++ Engine/Paddle.cpp
@@ -0,0 +1,56 @@
+#include "Paddle.h"
+
+Paddle::Paddle( Vec2 in_pos, float in_half_width, float in_half_height, Color in_color, float in_wing_width, Color in_wing_color)
+:
+	pos(in_pos),
+	halfWidth(in_half_width),
+	halfHeight(in_half_height),
+	c(in_color),
+	wingWidth(in_wing_width),
+	wingColor(in_wing_color)
+{
+}
+
+void Paddle::Update(const Keyboard & kbd, const float dt)
+{
+	if (kbd.KeyIsPressed(VK_LEFT) || kbd.KeyIsPressed(0x41))
+	{
+		pos.x -= speed * dt;
+	}
+	else if ( kbd.KeyIsPressed(VK_RIGHT) || kbd.KeyIsPressed(0x44))
+	{
+		pos.x += speed * dt;
+	}
+}
+
+void Paddle::Draw(Graphics & gfx) const
+{
+	gfx.DrawRect(GetRect(), c);
+}
+
+void Paddle::DoBallCollision(Ball & ball) const
+{
+	const RectF rect = GetRect();
+	if (rect.IsOverlappingWith(ball.GetRect()) && ball.GetVelocity().y >= 0)
+	{
+		ball.ReboundY();
+	}
+}
+
+void Paddle::DoWallCollision(const RectF & walls)
+{
+	const RectF rect = GetRect();
+	if (rect.left <= walls.left)
+	{
+		pos.x += walls.left - rect.left;
+	}
+	else if (rect.right >= walls.right)
+	{
+		pos.x -= rect.right - walls.right;
+	}
+}
+
+RectF Paddle::GetRect() const
+{
+	return RectF::FromCenter(pos, halfWidth, halfHeight);
+}
---
+++ Engine/Paddle.h
@@ -0,0 +1,29 @@
+#include "Ball.h"
+#include "RectF.h"
+#include "Colors.h"
+#include "Vec2.h"
+#include "Keyboard.h"
+#include "Graphics.h"
+
+class Paddle
+{
+public:
+	Paddle( Vec2 in_pos, float in_half_width, float in_half_height, Color in_color, float in_wing_width, Color in_wing_color);
+	void Update(const Keyboard& kbd, const float dt);
+	void Draw(Graphics& gfx) const;
+	void DoBallCollision(Ball& ball) const;
+	void DoWallCollision(const RectF& walls);
+	RectF GetRect() const;
+private:
+	float wingWidth;
+	float halfWidth;
+	float halfHeight;
+
+	Color c;
+	Color wingColor;
+
+	Vec2 pos;
+
+	static constexpr float speed = 230.0f;
+
+};
